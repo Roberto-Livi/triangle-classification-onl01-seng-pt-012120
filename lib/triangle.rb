c636e5fdccc2951ceefd8aed5926ad64deac5932
Roberto Livi <robertomlivi@outlook.com>
2020-02-16 18:33:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,10 +8,18 @@ class Triangle
   
   def kind
     if (@side_a <= 0) || (@side_b <= 0) || (@side_c <= 0)
+      begin
       raise TriangleError
+    rescue TriangleError => error
+      puts error.message
+    end
       
     elsif (@side_a + @side_b <= @side_c) || (@side_a + @side_c <= @side_b) || (@side_b + @side_c <= @side_a)
+      begin
       raise TriangleError
+    rescue TriangleError => error
+      puts error.message
+    end
       
     else
       if (@side_a == @side_b) && (@side_b == @side_c)
